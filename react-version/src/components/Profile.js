0c122e1d7030004b98615974f6bde585b0b3a27a
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-10 13:50:58 -0400

    Make components dynamic to be easily modified

--- react-version/src/components/Profile.js
+++ react-version/src/components/Profile.js
@@ -1,16 +1,18 @@
 function Profile() {
+  const firstName = "Khaled"
+  const lastName = "Alkhatib"
+  const avatar = "/profile-hex.png"
+
   return (
     <aside>
-    <div className="profile">
-      <img className="profile__image" src="./profile-hex.png" />
-    </div>
-    <br />
-    <div className="profile__name">
-      <h2><span className="profile--bold">Amy</span> Mansell</h2>
-    </div>
-  </aside>
-
-  );
+      <div className="profile">
+        <img className="profile__image" src={avatar} />
+      </div>
+      <br />
+      <div className="profile__name">
+        <h2><span className="profile--bold">{firstName}</span> {lastName}</h2>
+      </div>
+    </aside>
+  )
 }
-
 export default Profile;
